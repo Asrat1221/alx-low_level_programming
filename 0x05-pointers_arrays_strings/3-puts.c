bc69d7579e5af5487dceb4fda1ef5126d6d38b15
Asrat1221 <agtsehay@gmail.com>
2023-02-26 23:12:25 -0800

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -7,12 +7,13 @@
  */
 void _puts(char *str)
 {
-	int l = 0;
+        int l = 0;
 
-	while (*(str + l) != '\0')
-	{
-		_putchar(str[l]);
-		l++;
-	}
-	_putchar('\n');
+        while (*(str + l) != '\0')
+        {
+                _putchar(str[l]);
+                l++;
+        }
+        _putchar('\n');
 }
+
